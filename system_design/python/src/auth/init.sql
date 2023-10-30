bcb3d6ce715bd63f5a136818a5fd2d392449dafc
Roman-Ojha <razzroman98@gmail.com>
2023-10-30 18:31:52 +0545

    feat: added validate route to validate jwt with createJWT to create jwt

--- system_design/python/src/auth/init.sql
+++ system_design/python/src/auth/init.sql
@@ -1,19 +1,19 @@
 -- this user is actual user which will access our database not the user trying to give access to api
--- CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
 -- this is the user that we are creating to access the mysql Database
 -- but by default we have the username 'root' with blank password if you want to use that
 
 -- database for auth service
--- CREATE DATABASE video_to_mp3_ms_auth; 
+CREATE DATABASE video_to_mp3_ms_auth; 
 
 -- granting all the privileges for auth tables to 'auth_user'@'localhost'
--- GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
--- USE video_to_mp3_ms_auth;
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+USE video_to_mp3_ms_auth;
 
 -- creating table
 CREATE TABLE user(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
--- system_design/python/src/auth/server.py
+++ system_design/python/src/auth/server.py
@@ -24,6 +24,7 @@ server.config["MYSQL_USER"] = config("MYSQL_USER")
 server.config["MYSQL_PASSWORD"] = config("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = config("MYSQL_DB")
 server.config["MYSQL_PORT"] = config("MYSQL_PORT")
+# added all of the configuration variable here for the MySQL database connection
 
 
 # First route
@@ -36,4 +37,67 @@ def login():
     if not auth:
         return "missing credentials", 401
 
+    # Basic Authentication Scheme
+    # Authorization: Basic base64(<username>:<password>)
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Authorization
+
     # check db for username and password
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+        "SELECT email, password FROM user WHERE email=%s", (auth.username,)
+    )
+    if res > 0:
+        # if user exist
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+        if auth.username != email or auth.password != password:
+            return "Invalid credentials", 401
+        else:
+            return createJWT(auth.username, config("JWT_SECRET"), True)
+    else:
+        return "Invalid Credentials", 401
+
+
+# route which will validate the JWT coming from the 'Authorization' Header
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+
+    # Bearer Authentication Scheme
+    # Authorization: Bearer <token>
+
+    if not encoded_jwt:
+        return "missing credentials", 401
+
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, config("JWT_SECRET"), algorithms=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+
+    return decoded, 200
+
+
+def createJWT(username, secret, authz):
+    # authz is something that identify whether user is admin or not
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1),
+            # token expire in 1 days
+            "iat": datetime.datetime.utcnow(),  # issued at
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256",  # single private key algorithm
+    )
+
+
+if __name__ == "__main__":
+    # running the server on public ip
+    server.run(host="0.0.0.0", port=5000)
+    # Explanation: https://youtu.be/hmkF77F9TLw?t=2631
