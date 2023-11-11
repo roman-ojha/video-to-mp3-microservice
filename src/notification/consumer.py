97f46a0b00e06db639051e073067a1753ba59d80
Roman-Ojha <razzroman98@gmail.com>
2023-11-11 19:55:15 +0545

    feat: completed notification service logic

---
+++ src/notification/consumer.py
@@ -0,0 +1,39 @@
+import pika
+import sys
+import os
+import time
+# this 'send' package from './send' with file './send/email.py' help to send email notification to the client
+from send import email
+
+
+def main():
+    # rabbitmq connection
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="rabbitmq"))
+    channel = connection.channel()
+
+    def callback(ch, method, properties, body):
+        err = email.notification(body)
+        if err:
+            ch.basic_nack(delivery_tag=method.delivery_tag)
+        else:
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_consume(
+        queue=os.environ.get("MP3_QUEUE"), on_message_callback=callback
+    )
+
+    print("Waiting for messages. To exit press CTRL+C")
+
+    channel.start_consuming()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
